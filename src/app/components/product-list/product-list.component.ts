6c6386f72b9bf659c6f224ae0111eab5a03090a5
uspehgp <uspehgp@gmail.com>
2021-10-21 11:43:14 +0300

    inject categoryId and activatedRoute at product-list.component

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,26 +1,37 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../../services/product.service";
 import {Product} from "../../common/product";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-  // templateUrl: './product-list-table.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
 
   products: Product[] | undefined;
+  currentCategoryId: number | undefined;
 
-  constructor(private productService: ProductService) {
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
-    this.listProducts()
+    this.route.paramMap.subscribe(() => {
+      this.listProducts()
+    })
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe(
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+    if (hasCategoryId) {
+      // @ts-ignore
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
+    }
+    else this.currentCategoryId =1;
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
         this.products = data
       }
