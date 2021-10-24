5d9b856f8dd14c5cd04bd5935a8c25828702af73
uspehgp <uspehgp@gmail.com>
2021-10-24 18:28:40 +0300

    add Selecting Page Size

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -16,7 +16,7 @@ export class ProductListComponent implements OnInit {
   searchMode: boolean = false;
 
   thePageNumber: number = 1;
-  thePageSize: number = 10;
+  thePageSize: number = 12;
   theTotalElements: number = 0;
 
 
@@ -85,5 +85,11 @@ export class ProductListComponent implements OnInit {
       this.thePageSize = data.page.size;
       this.theTotalElements = data.page.totalElements;
     };
+  }
+
+  updatePageSize(pageSize: number) {
+    this.thePageSize = pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
   }
 }
