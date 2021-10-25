089a90cf5f80a05d1ee2ad8f9ca32f1561dad4ab
Uspehgp <uspehgp@gmail.com>
2021-10-25 18:52:18 +0300

    Create add to cart method

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -97,4 +97,8 @@ export class ProductListComponent implements OnInit {
     this.thePageNumber = 1;
     this.listProducts();
   }
+
+  addToCart(theProduct: Product) {
+    console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+  }
 }
