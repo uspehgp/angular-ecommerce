bb9661a61e1300087ee12631aea983687b9e98dc
Uspehgp <uspehgp@gmail.com>
2021-10-26 15:42:43 +0300

    call cart service by addToCart() from product list component

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {ProductService} from 'src/app/services/product.service';
 import {Product} from 'src/app/common/product';
 import {ActivatedRoute} from '@angular/router';
+import {CartItem} from 'src/app/common/cart-item';
+import {CartService} from "../../services/cart.service";
+
 
 @Component({
   selector: 'app-product-list',
@@ -15,6 +18,7 @@ export class ProductListComponent implements OnInit {
   previousCategoryId: number = 1;
   searchMode: boolean = false;
 
+  // new properties for pagination
   thePageNumber: number = 1;
   thePageSize: number = 12;
   theTotalElements: number = 0;
@@ -22,8 +26,8 @@ export class ProductListComponent implements OnInit {
   previousKeyword: string = null;
 
   constructor(private productService: ProductService,
-              private route: ActivatedRoute) {
-  }
+              private cartService: CartService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(() => {
@@ -37,7 +41,8 @@ export class ProductListComponent implements OnInit {
 
     if (this.searchMode) {
       this.handleSearchProducts();
-    } else {
+    }
+    else {
       this.handleListProducts();
     }
 
@@ -47,16 +52,22 @@ export class ProductListComponent implements OnInit {
 
     const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
 
+    // if we have a different keyword than previous
+    // then set thePageNumber to 1
+
     if (this.previousKeyword != theKeyword) {
       this.thePageNumber = 1;
     }
 
     this.previousKeyword = theKeyword;
-    console.log(`keyword=${theKeyword}, pagenumber=${this.thePageNumber}`);
+
+    console.log(`keyword=${theKeyword}, thePageNumber=${this.thePageNumber}`);
 
     // now search for the products using keyword
-    this.productService.searchProductsPaginate(this.thePageNumber - 1, this.thePageSize, theKeyword)
-      .subscribe(this.processResult())
+    this.productService.searchProductsPaginate(this.thePageNumber - 1,
+      this.thePageSize,
+      theKeyword).subscribe(this.processResult());
+
   }
 
   handleListProducts() {
@@ -67,23 +78,35 @@ export class ProductListComponent implements OnInit {
     if (hasCategoryId) {
       // get the "id" param string. convert string to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id');
-    } else {
+    }
+    else {
       // not category id available ... default to category id 1
       this.currentCategoryId = 1;
     }
 
+    //
+    // Check if we have a different category than previous
+    // Note: Angular will reuse a component if it is currently being viewed
+    //
+
+    // if we have a different category id than previous
+    // then set thePageNumber back to 1
     if (this.previousCategoryId != this.currentCategoryId) {
       this.thePageNumber = 1;
     }
+
     this.previousCategoryId = this.currentCategoryId;
+
     console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
 
     // now get the products for the given category id
-    this.productService.getProductListPaginate(this.thePageNumber - 1, this.thePageSize, this.currentCategoryId)
+    this.productService.getProductListPaginate(this.thePageNumber - 1,
+      this.thePageSize,
+      this.currentCategoryId)
       .subscribe(this.processResult());
   }
 
-  private processResult() {
+  processResult() {
     return data => {
       this.products = data._embedded.products;
       this.thePageNumber = data.page.number + 1;
@@ -99,6 +122,12 @@ export class ProductListComponent implements OnInit {
   }
 
   addToCart(theProduct: Product) {
+
     console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+
+    const theCartItem = new CartItem(theProduct);
+
+    this.cartService.addToCart(theCartItem);
   }
+
 }
