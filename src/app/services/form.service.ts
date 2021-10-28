23ca012912f06683dbc46dca8c0343ce53ccaf84
Uspehgp <uspehgp@gmail.com>
2021-10-28 20:25:07 +0300

    create form service

---
+++ src/app/services/form.service.ts
@@ -0,0 +1,29 @@
+import {Injectable} from '@angular/core';
+import {Observable, of} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FormService {
+
+  constructor() {
+  }
+
+  getCreditCardMonths(startMonth: number): Observable<number[]> {
+
+    let data: number[] = [];
+    for (let theMonth = startMonth; theMonth <= 12; theMonth++) {
+      data.push(theMonth);
+    }
+    return of(data);
+  }
+
+  getCreditCardYears(): Observable<number[]> {
+
+    let data: number[] = [];
+    for (let theYear = new Date().getFullYear(); theYear <= theYear + 10; theYear++) {
+      data.push(theYear);
+    }
+    return of(data);
+  }
+}
