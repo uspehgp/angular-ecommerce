2e52ad4ab6403c1587b8ae409f2081b5f25a16ce
Uspehgp <uspehgp@gmail.com>
2021-10-30 14:35:35 +0300

    add methods to the form service for Country and State

--- src/app/services/form.service.ts
+++ src/app/services/form.service.ts
@@ -1,12 +1,44 @@
 import {Injectable} from '@angular/core';
 import {Observable, of} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {Country} from "../common/country";
+import {map} from "rxjs/operators";
+import {State} from "../common/state";
+
+interface GetResponseCounties {
+  _embedded: {
+    countries: Country[];
+  }
+}
+
+interface GetResponseStates {
+  _embedded: {
+    states: State[];
+  }
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormService {
 
-  constructor() {
+  private countriesUrl = 'http://localhost:8080/api/countries'
+  private statesUrl = 'http://localhost:8080/api/states'
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getCountries(): Observable<Country[]> {
+    return this.httpClient.get<GetResponseCounties>(this.countriesUrl).pipe(
+      map(response => response._embedded.countries)
+    );
+  }
+
+  getStates(theCountryCode: string): Observable<State[]> {
+    const getSearchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${theCountryCode}`
+    return this.httpClient.get<GetResponseStates>(getSearchStatesUrl).pipe(
+      map(response => response._embedded.states)
+    );
   }
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
