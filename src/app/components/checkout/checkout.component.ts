37a8d22a95114127de911d55ed10a62a835ae31f
Uspehgp <uspehgp@gmail.com>
2021-11-01 21:09:04 +0300

    create custom validation for first and last name

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -3,6 +3,7 @@ import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {FormService} from "../../services/form.service";
 import {Country} from "../../common/country";
 import {State} from "../../common/state";
+import {MyValidators} from "../../validators/my-validators";
 
 @Component({
   selector: 'app-checkout',
@@ -32,8 +33,8 @@ export class CheckoutComponent implements OnInit {
 
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
-        lastName:  new FormControl('', [Validators.required, Validators.minLength(2)]),
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2), MyValidators.notOnlyWhitespace]),
+        lastName:  new FormControl('', [Validators.required, Validators.minLength(2), MyValidators.notOnlyWhitespace]),
         email: new FormControl('',
           [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
